ee3acc8c9e08c12e86a149bc06ec1cdcc6e7d228
chikchilcy <chikchicly@gmail.com>
2024-10-05 10:28:45 +0500

    meneshOpening

--- site/js/main.js
+++ site/js/main.js
@@ -7,9 +7,25 @@ document.querySelector('#menuSh').addEventListener('click',()=>{
         document.querySelector('#menuSh > span:nth-child(2)').style.cssText="display:none";
     document.querySelector('#menuSh > span:nth-child(1)').style.cssText="margin:3,4px";
     document.querySelector('#menuSh > span:nth-child(3)').style.cssText="margin-top:3,5px";
+    let openMenuSh=document.createElement('div');
+    openMenuSh.style.cssText=`
+        display:flex;
+        width:100%;
+        height:100vh;
+        backround:white;
+        justify-content:center;
+        align-items:center;
+        font-size:30px;
+    `;
+    openMenuSh.setAttribute('id','menuShOpening');
+    openMenuSh.innerHTML='There will be a menu here';
+    document.querySelector('main').prepend(openMenuSh);
+    document.querySelector('#menuShOpening').addEventListener('click',()=>{
+        document.querySelector('#menuShOpening').remove();
+    });
 
     }else if(siteStatus.click==2){
-        console.log('two')
+        console.log('two');
         document.querySelector('#menuSh > span:nth-child(2)').style.cssText="display:block";
         // let newLine=document.createElement('span');
         // document.querySelector('#menuSh > span:nth-child(1)').append(newLine);
