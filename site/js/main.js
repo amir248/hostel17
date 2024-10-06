8145c2121ab4179a69cf4f6f921b9bc8f9ce16a4
chikchilcy <chikchicly@gmail.com>
2024-10-06 08:15:53 +0500

    menu is open

--- site/js/main.js
+++ site/js/main.js
@@ -4,31 +4,44 @@ document.querySelector('#menuSh').addEventListener('click',()=>{
     console.log('oK'+ siteStatus.click);
     siteStatus.click++;
     if(siteStatus.click==1){
-        document.querySelector('#menuSh > span:nth-child(2)').style.cssText="display:none";
-    document.querySelector('#menuSh > span:nth-child(1)').style.cssText="margin:3,4px";
-    document.querySelector('#menuSh > span:nth-child(3)').style.cssText="margin-top:3,5px";
+        document.querySelector('body').style.cssText='overflow:hidden;';
+        document.querySelector('#menuSh > span:nth-child(2)').style.cssText="opacity:0;display:none;transition:ease-out 1s;";
+        document.querySelector('#menuSh > span:nth-child(1)').style.cssText="margin:3,4px; transform:rotate(35deg);transition:ease-out 1s;";
+        document.querySelector('#menuSh > span:nth-child(3)').style.cssText="margin-top:3,5px;transform:rotate(-35deg);transition:ease-out 1s;";
     let openMenuSh=document.createElement('div');
     openMenuSh.style.cssText=`
         display:flex;
         width:100%;
         height:100vh;
-        backround:white;
+        background:rgba(255,255,255,0.5);
+        position:fixed;
         justify-content:center;
         align-items:center;
         font-size:30px;
+        z-index:99;
+        top:0;
+        transition:ease-out 1s;
+        flex-direction:column;
     `;
     openMenuSh.setAttribute('id','menuShOpening');
-    openMenuSh.innerHTML='There will be a menu here';
+    openMenuSh.innerHTML='There will be a menu here<br><a href="/">home</a> <a href="/#oK">first Link</a>';
     document.querySelector('main').prepend(openMenuSh);
     document.querySelector('#menuShOpening').addEventListener('click',()=>{
+        document.querySelector('body').style.cssText=`overflow:auto;`;
         document.querySelector('#menuShOpening').remove();
+        siteStatus.click=0;
+        document.querySelector('#menuSh > span:nth-child(2)').style.cssText=`
+        display:block;
+        transition:ease-out 1s;
+        opacity:1;
+        `;
+        document.querySelector('#menuSh > span:nth-child(1)').style.cssText="margin:3,4px; transform:rotate(0deg);transition:ease-out 1s;";
+        document.querySelector('#menuSh > span:nth-child(3)').style.cssText="margin-top:3,5px;transform:rotate(0deg);transition:ease-out 1s;";
     });
 
     }else if(siteStatus.click==2){
         console.log('two');
-        document.querySelector('#menuSh > span:nth-child(2)').style.cssText="display:block";
         // let newLine=document.createElement('span');
         // document.querySelector('#menuSh > span:nth-child(1)').append(newLine);
-        siteStatus.click=0;
     }
 })
